57c8712531b1ec8af3755147c84038299688ee75
elshiraphine <erviana.205025@mhs.its.ac.id>
2024-05-28 16:53:08 +0700

    feat: add login screen view

---
+++ lib/components/my_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final Function()? onTap;
+  final String text;
+
+  const MyButton({super.key, required this.onTap, required this.text});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+          padding: const EdgeInsets.all(20.0),
+          margin: const EdgeInsets.symmetric(horizontal: 25.0),
+          decoration: BoxDecoration(
+              color: Colors.red.shade900,
+              borderRadius: BorderRadius.circular(5)
+          ),
+          child: Center(
+              child: Text(
+                text,
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold
+                ),
+              ))
+      ),
+    );
+  }
+}
