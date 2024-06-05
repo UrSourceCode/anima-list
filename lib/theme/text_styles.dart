8d530bb9e9b90884cf9d8ac5d5b1b6b3c89b643a
elshiraphine <erviana.205025@mhs.its.ac.id>
2024-06-06 04:30:55 +0700

    feat: create and initialize app theme

---
+++ lib/theme/text_styles.dart
@@ -0,0 +1,41 @@
+// lib/theme/text_styles.dart
+
+import 'package:anima_list/theme/colors.dart';
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle displayLarge = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+    color: AppColors.lightPrimaryColor
+  );
+
+  static const TextStyle displayMedium = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: AppColors.lightPrimaryColor
+  );
+
+  static const TextStyle titleLarge = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: AppColors.onLightBackgroundColor
+  );
+
+  static const TextStyle titleMedium = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+    color: AppColors.onLightBackgroundColor
+  );
+
+  static const TextStyle bodyMedium = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+    color: AppColors.onLightBackgroundColor
+  );
+}
