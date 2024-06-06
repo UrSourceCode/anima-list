5b37964e6f32a15970ab4516a16e91441a6b8767
elshiraphine <erviana.205025@mhs.its.ac.id>
2024-06-07 03:13:28 +0700

    refactor: move the directory from page to screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,35 +1,49 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class CommunityScreen extends StatefulWidget {
-  const CommunityScreen({super.key});
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
 
   @override
-  State<CommunityScreen> createState() => _CommunityScreenState();
+  State<ProfileScreen> createState() => _ProfileScreenState();
 }
 
-class _CommunityScreenState extends State<CommunityScreen> {
+class _ProfileScreenState extends State<ProfileScreen> {
+  void signUserOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black87,
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: signUserOut,
+          ),
+        ],
+      ),
+      backgroundColor: Colors.black,
       body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
-                children: [
+                children:[
                   // SizedBox(height: 60),
-
                   Text(
-                    'Community Page',
+                    'Profile Page',
                     style: TextStyle(
                       fontSize: 24,
                       color: Colors.red.shade900,
                       fontWeight: FontWeight.bold,
                     ),
                     textAlign: TextAlign.left,
-                  )
-                ]
+                  ),
+
+                  // SizedBox(height: 20),
+                ],
             ),
           )
       )
